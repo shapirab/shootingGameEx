b216d5892ce8bce7c4cb9b1f336b2a9f5928188e
shapirab <shapirab@jamd.ac.il>
2023-04-02 08:48:11 +0300

    2/4/23 remove projectile when out of bounds

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -11,27 +11,44 @@ let center = {
 }
 let player = new Object(center, 30, 'white', null);
 let angle = 0;
-let particles = [];
+let projectiles = [];
 
 window.addEventListener('click', (e) => {
     angle = Math.atan2(e.clientY - center.y, e.clientX - center.x);
+    let velocityFactor = 5;
     let velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
+        x: Math.cos(angle) * velocityFactor,
+        y: Math.sin(angle) * velocityFactor
     };
-    let particle = new Object(center, 5, 'gold', velocity);
-    particles.push(particle);
+    let projectileColor = 'rgb(255, 255, 255)';
+    let projectileRadius = 3;
+    let projectile = new Object(center, projectileRadius, projectileColor, velocity);
+    projectiles.push(projectile);
     
 });
+
+function removeProjectileIfOutOfBounds(projectile, index){
+    if(projectileOutOfBounds(projectile)){
+        projectiles.splice(index, 1);
+    }
+}
+
+function projectileOutOfBounds(projectile){
+    return projectile.position.x > canvas.width ||
+            projectile.position.x < 0 || 
+            projectile.position.y > canvas.height ||
+            projectile.position.y < 0;
+}
 
 function animate(){
     requestAnimationFrame(animate);
     ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     player.draw(ctx);
-    particles.forEach(particle => {
-        particle.update();
-        particle.draw(ctx);
+    projectiles.forEach((projectile, index) => {
+        removeProjectileIfOutOfBounds(projectile, index);
+        projectile.update();
+        projectile.draw(ctx);
     });
 }
 
